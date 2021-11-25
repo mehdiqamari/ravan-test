394b5a519438721d8214e547cec0e7f68108afe2
mehdiqamari <82534612+mehdiqamari@users.noreply.github.com>
2021-11-25 02:32:06 -0800

    create test box

---
+++ src/components/TestBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="MinBox"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.MinBox {
+  background-color: rgb(245, 245, 245);
+  height: 500px;
+  width: 700px;
+  border-radius: 8px;
+  background-color: rgb(246, 247, 248);
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+}
+</style>
