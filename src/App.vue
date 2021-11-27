1b8e2221dce1d0d94c40071a60d8a1555a37741f
mehdiqamari <82534612+mehdiqamari@users.noreply.github.com>
2021-11-26 22:57:06 -0800

    create paginasion

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
 <template>
-  <div>
-      <tset-box v-bind:a="a"/>
-          
+  <div dir="rtl">
+    <tset-box class="test-box" v-bind:a="listtoshow" />
+    <paginision
+      :page="curntpage"
+      @next="curntpage += 1"
+      @befor="curntpage -= 1"
+     
+    />
   </div>
 </template>
 
 <script>
 import { a } from "./data.js";
 import TsetBox from "./components/TestBox.vue";
+import Paginision from "./components/pag.vue";
 export default {
   components: {
     TsetBox,
+    Paginision,
   },
   data() {
     return {
-      a
-    }
+      a,
+      curntpage: 1,
+      itemperpage: 1,
+    };
+  },
+  computed: {
+    listtoshow() {
+      return this.a.slice(
+        (this.curntpage - 1) * this.itemperpage,
+        this.curntpage * this.itemperpage
+      )
+    },
   },
 };
 </script>
 
-<style>
-body{
-    background-image: url("assets/1.jpg");
-    background-repeat: no-repeat;
+<style  scoped>
+body {
+  background-image: url("assets/1.jpg");
     background-position: center;
-    background-attachment: fixed;
-    background-size: 100%px 100%px;
+  background-attachment: fixed;
+  background-size: 100%px 100%px;
+}
+.test-box{
+  margin-top: 100px;
+  margin-right: 100px;
 }
 </style>
---
+++ src/components/pag.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button v-if="page > 1 " v-on:click="gotobefor">قبلی</button>
+    <button v-if="page < 30" v-on:click="gotonext">بعدی</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    page: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    gotonext() {
+      this.$emit("next");
+    },
+    gotobefor() {
+      this.$emit("befor");
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  height: 60px;
+  width: 150px;
+  background-color: rgb(180, 246, 204);
+  border: none;
+  border-radius: 5px;
+  margin: 50px 50px 50px 50px;
+}
+</style>
